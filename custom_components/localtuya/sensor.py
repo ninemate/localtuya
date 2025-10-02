0ac4e0206882124a60fca6575002c30474e0dca8
ninemate <ninemate@mgmt1.sznszkmnet>
2025-10-02 23:32:11 +0200

    I left out a )

--- custom_components/localtuya/sensor.py
+++ custom_components/localtuya/sensor.py
@@ -89,4 +89,4 @@ class LocaltuyaSensor(LocalTuyaEntity):
         return
 
 
-async_setup_entry = partial(async_setup_entry, DOMAIN, LocaltuyaSensor, flow_schema
+async_setup_entry = partial(async_setup_entry, DOMAIN, LocaltuyaSensor, flow_schema)
